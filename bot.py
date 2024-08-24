88693b3ca239cea733fa1a94209f42194a1aab37
AlexTheMaggot <alexthemaggot23@gmail.com>
2024-08-24 16:38:23 +0500

    - add states and set language handler

--- bot.py
+++ bot.py
@@ -6,7 +6,10 @@ from aiogram.filters import CommandStart
 from aiogram.types import Message
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
+from aiogram.fsm.state import State, StatesGroup
+from aiogram.fsm.context import FSMContext
 
+import kb
 from config import *
 
 
@@ -14,9 +17,30 @@ TOKEN = TELEGRAM_TOKEN
 dp = Dispatcher()
 
 
+class Menu(StatesGroup):
+    lang = State()
+    name = State()
+
+
 @dp.message(CommandStart())
-async def command_start_handler(message: Message):
-    await message.answer('Command start')
+async def command_start_handler(message: Message, state: FSMContext):
+    await state.set_state(Menu.lang)
+    await message.answer('Приветствие и выбор языка', reply_markup=kb.start_kb)
+
+
+@dp.message(Menu.lang)
+async def language_handler(message: Message, state: FSMContext):
+    match message.text:
+        case 'Русский':
+            await state.update_data(lang='ru')
+            await state.set_state(Menu.name)
+            await message.answer(text='Пожалуйста, укажите имя')
+        case 'Узбекский':
+            await state.update_data(lang='uz')
+            await state.set_state(Menu.name)
+            await message.answer(text='Пожалуйста, укажите имя(на узбекском)')
+        case _:
+            await message.answer(text='Пожалуйста укажите язык (рус и узб)', reply_markup=kb.start_kb)
 
 
 @dp.message()
