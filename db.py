903a26e5dc837a2920de61653b1dcb48dbf6a09e
AlexTheMaggot <alexthemaggot23@gmail.com>
2024-09-08 20:33:55 +0500

    - add region and about update in users

--- db.py
+++ db.py
@@ -15,11 +15,14 @@ def user_get_detail(user_id):
         'user_id': fetch[0],
         'lang': fetch[1],
         'name': fetch[2],
+        'phone_number': fetch[3],
+        'region': fetch[4],
+        'about': fetch[5],
     }
     return user
 
 
-def user_update(user_id, lang=None, name=None, phone_number=None):
+def user_update(user_id, lang=None, name=None, phone_number=None, region=None, about=None):
     if lang:
         cursor.execute(f'''UPDATE users SET lang = '{lang}' ''')
         conn.commit()
@@ -29,4 +32,9 @@ def user_update(user_id, lang=None, name=None, phone_number=None):
     if phone_number:
         cursor.execute(f'''UPDATE users SET phone_number = '{phone_number}' ''')
         conn.commit()
-
+    if region:
+        cursor.execute(f'''UPDATE users SET region = '{region}' ''')
+        conn.commit()
+    if about:
+        cursor.execute(f'''UPDATE users SET about = '{about}' ''')
+        conn.commit()
