2683c3648143ac2a671329d0c42c8f33303086ef
AlexTheMaggot <alexthemaggot23@gmail.com>
2024-09-09 16:20:24 +0500

    - fix get detail bug and update bug

--- db.py
+++ db.py
@@ -11,30 +11,31 @@ def user_create(user_id):
 
 def user_get_detail(user_id):
     fetch = cursor.execute(f'''SELECT * FROM users WHERE user_id = {user_id}''').fetchone()
-    user = {
-        'user_id': fetch[0],
-        'lang': fetch[1],
-        'name': fetch[2],
-        'phone_number': fetch[3],
-        'region': fetch[4],
-        'about': fetch[5],
-    }
-    return user
+    if fetch:
+        user = {
+            'user_id': fetch[0],
+            'lang': fetch[1],
+            'name': fetch[2],
+            'phone_number': fetch[3],
+            'region': fetch[4],
+            'about': fetch[5],
+        }
+        return user
 
 
 def user_update(user_id, lang=None, name=None, phone_number=None, region=None, about=None):
     if lang:
-        cursor.execute(f'''UPDATE users SET lang = '{lang}' ''')
+        cursor.execute(f'''UPDATE users SET lang = '{lang}' WHERE user_id = {user_id}''')
         conn.commit()
     if name:
-        cursor.execute(f'''UPDATE users SET name = '{name}' ''')
+        cursor.execute(f'''UPDATE users SET name = '{name}' WHERE user_id = {user_id}''')
         conn.commit()
     if phone_number:
-        cursor.execute(f'''UPDATE users SET phone_number = '{phone_number}' ''')
+        cursor.execute(f'''UPDATE users SET phone_number = '{phone_number}' WHERE user_id = {user_id}''')
         conn.commit()
     if region:
-        cursor.execute(f'''UPDATE users SET region = '{region}' ''')
+        cursor.execute(f'''UPDATE users SET region = '{region}' WHERE user_id = {user_id}''')
         conn.commit()
     if about:
-        cursor.execute(f'''UPDATE users SET about = '{about}' ''')
+        cursor.execute(f'''UPDATE users SET about = '{about}' WHERE user_id = {user_id}''')
         conn.commit()
